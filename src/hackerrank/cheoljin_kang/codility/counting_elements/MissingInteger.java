8f3ad812a2fecdc42312276ee8c0751a205f75cd
CheolJin Kang <kcj091857@gmail.com>
2018-08-24 18:23:48 +0900

    Resolve algorithm Codility MissingInteger

--- src/hackerrank/cheoljin_kang/codility/counting_elements/MissingInteger.java
+++ src/hackerrank/cheoljin_kang/codility/counting_elements/MissingInteger.java
@@ -1,4 +1,51 @@
 package hackerrank.cheoljin_kang.codility.counting_elements;
 
 public class MissingInteger {
+    public static void main(String[] args) {
+        int[] a1 = {1, 3, 6, 4, 1, 2}; // 5
+        int[] a2 = {1, 2, 3}; // 4
+        int[] a3 = {-1, -3}; // 1
+        int[] a4 = {1}; // 2
+        System.out.println(solution(a1));
+    }
+
+    static int solution(int[] A) {
+        int minValue = A[0];
+        int maxValue = A[0];
+        int result = -1;
+        if (A.length == 1) {
+            if (A[0] == 1) {
+                result = 2;
+            } else {
+                result = 1;
+            }
+        } else {
+            for (int i = 0; i < A.length; i++) {
+                if (A[i] < minValue) {
+                    minValue = A[i];
+                }
+                if (A[i] > maxValue) {
+                    maxValue = A[i];
+                }
+            }
+            if (minValue > 1 || maxValue < 1) {
+                result = 1;
+            } else {
+                boolean[] array = new boolean[maxValue + 1];
+                for (int i = 0; i < A.length; i++) {
+                    array[A[i]] = true;
+                }
+                for (int i = 1; i < array.length; i++) {
+                    if (array[i] == false) {
+                        result = i;
+                        break;
+                    }
+                }
+                if (result == -1) {
+                    result = maxValue + 1;
+                }
+            }
+        }
+        return result;
+    }
 }
--- src/hackerrank/mkh/CountPerfectSubtree.java
+++ src/hackerrank/mkh/CountPerfectSubtree.java
@@ -7,84 +7,84 @@ import java.util.NoSuchElementException;
 public class CountPerfectSubtree {
 	public static void main(String[] args) {
 		Tree tree = new Tree(1);
-		
+
 		// test case 1
 		// left
 		Tree tree2 = new Tree(2);
 		tree2.rightInsert(new Tree(4));
 		tree.leftInsert(tree2);
-		
+
 		// right
 		Tree tree3 = new Tree(3);
 		Tree tree5 = new Tree(5);
 		tree5.leftInsert(new Tree(7));
 		tree5.rightInsert(new Tree(8));
 		tree3.leftInsert(tree5);
-		
+
 		Tree tree10 = new Tree(10);
-		tree10.leftInsert(new Tree(11));	
+		tree10.leftInsert(new Tree(11));
 		Tree tree6 = new Tree(6);
 		tree6.rightInsert(tree10);
 		tree6.leftInsert(new Tree(9));
 		tree3.rightInsert(tree6);
-		
+
 		tree.rightInsert(tree3);
-		
-		
+
+
 //		// test case 2
 //		tree.rightInsert(new Tree(3));
-//		
+//
 //		Tree t4 = new Tree(4);
 //		t4.leftInsert(new Tree(6));
 //		t4.rightInsert(new Tree(7));
-//		
+//
 //		Tree t5 = new Tree(5);
 //		t5.leftInsert(new Tree(8));
 //		t5.rightInsert(new Tree(9));
-//		
+//
 //		Tree t2 = new Tree(2);
 //		t2.leftInsert(t4);
 //		t2.rightInsert(t5);
-//		
+//
 //		tree.leftInsert(t2);
 ////		tree.preorder(tree);
-		
+
 		List<Integer> list = new ArrayList<>();
 		tree.countTree(1, list, tree);
-	
+
 		int max = list.stream().mapToInt(i -> i).max().orElseThrow(NoSuchElementException::new);
 		System.out.println(max);
-		
+
 	}
-	
-	
+
+
 }
 
 class Tree {
 	public int x;
 	public Tree l;
 	public Tree r;
-	
+
 	public Tree(int x) {
 		this.x = x;
 		this.l = null;
 		this.r = null;
 	}
-	
+
 	public Tree leftInsert(Tree t) {
 		this.x = x;
 		this.l = t;
-		
+
 		return this.l;
 	}
-	
+
 	public Tree rightInsert(Tree t) {
 		this.x = x;
 		this.r = t;
-		
+
 		return this.r;
 	}
-	
+
 	public void countTree(int count, List<Integer> list, Tree t) {
 		if(t!=null) {
 			if(t.l != null && t.r != null) {
@@ -100,7 +100,7 @@ class Tree {
 			countTree(count, list, t.r);
 		}
 	}
-	
+
 	public void preorder(Tree t) {
 		if(t!=null) {
 			System.out.println(t.x);
