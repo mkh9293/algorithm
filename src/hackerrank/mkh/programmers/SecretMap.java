19739effe620890d216c71ee6a13aca3a14d4c4a
GwangHun Moon <mkh9293@gmail.com>
2018-09-10 23:12:46 +0900

    update secretMap code

--- src/hackerrank/mkh/programmers/SecretMap.java
+++ src/hackerrank/mkh/programmers/SecretMap.java
@@ -6,7 +6,6 @@ public class SecretMap {
 	public static void main(String[] args) {
 		int[] arr1 = {46, 33, 33, 22, 31, 50};
 		int[] arr2 = {27, 56, 19, 14, 14, 10};
-//		System.out.println(Arrays.toString(newSolution(6,arr1, arr2)));
 		newSolution(6,arr1, arr2);
 	}
 	
@@ -15,74 +14,48 @@ public class SecretMap {
 		for(int i=0; i<n; i++) {
 			result[i] = Integer.toBinaryString(arr1[i] | arr2[i]).replace('0', ' ').replace('1', '#');
 			while(result[i].length() != n) result[i] = ' ' + result[i];
-			
-			System.out.println(result[i]);
 		}
 		
 		return result;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	static String[] solution(int n, int[] arr1, int[] arr2) {
-		String[][] map = new String[n][n];
-		
-		map = settingMap(n, map, arr1);
-		map = settingMap(n, map, arr2);
-
-		String[] answer = new String[n];
-		for(int i=0; i<n; i++) {
-			StringBuilder temp = new StringBuilder();
-			for(int j=0; j<n; j++) {
-				temp.append(map[i][j]);
-			}
-			answer[i] = temp.toString();
-		}
-		
-		System.out.println(Arrays.toString(answer));
-		return answer;
-	}
-	
-	static String[][] settingMap(int n, String[][] map, int[] arr) {
-		for(int i=0; i<n; i++) {
-			StringBuilder temp = new StringBuilder(Integer.toBinaryString(arr[i]));
-			while(temp.length() < n) {
-				temp.insert(0,0);
-			}
-			
-			for(int j=0; j<n; j++) {
-				char c = temp.charAt(j);
-				if(c == '1' || (map[i][j] != null && map[i][j].equals("#"))) {
-					map[i][j] = "#";	
-				} else {
-					map[i][j] = " ";
-				}
-			}
-		}
-		StringBuilder s= new StringBuilder("*");
-		
-		return map;
-	}
+//	static String[] solution(int n, int[] arr1, int[] arr2) {
+//		String[][] map = new String[n][n];
+//		
+//		map = settingMap(n, map, arr1);
+//		map = settingMap(n, map, arr2);
+//
+//		String[] answer = new String[n];
+//		for(int i=0; i<n; i++) {
+//			StringBuilder temp = new StringBuilder();
+//			for(int j=0; j<n; j++) {
+//				temp.append(map[i][j]);
+//			}
+//			answer[i] = temp.toString();
+//		}
+//		
+//		System.out.println(Arrays.toString(answer));
+//		return answer;
+//	}
+//	
+//	static String[][] settingMap(int n, String[][] map, int[] arr) {
+//		for(int i=0; i<n; i++) {
+//			StringBuilder temp = new StringBuilder(Integer.toBinaryString(arr[i]));
+//			while(temp.length() < n) {
+//				temp.insert(0,0);
+//			}
+//			
+//			for(int j=0; j<n; j++) {
+//				char c = temp.charAt(j);
+//				if(c == '1' || (map[i][j] != null && map[i][j].equals("#"))) {
+//					map[i][j] = "#";	
+//				} else {
+//					map[i][j] = " ";
+//				}
+//			}
+//		}
+//		StringBuilder s= new StringBuilder("*");
+//		
+//		return map;
+//	}
 }
