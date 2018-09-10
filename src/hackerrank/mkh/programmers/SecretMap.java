23a9a9c10d370dc0a9d413da67918acf454c8722
GwangHun Moon <mkh9293@gmail.com>
2018-09-10 23:11:51 +0900

    update secretMap code

--- src/hackerrank/mkh/programmers/SecretMap.java
+++ src/hackerrank/mkh/programmers/SecretMap.java
@@ -4,10 +4,47 @@ import java.util.Arrays;
 
 public class SecretMap {
 	public static void main(String[] args) {
-		int[] arr1 = {9, 20, 28, 18, 11};
-		int[] arr2 = {30, 1, 21, 17, 28};
-		solution(5,arr1, arr2);
+		int[] arr1 = {46, 33, 33, 22, 31, 50};
+		int[] arr2 = {27, 56, 19, 14, 14, 10};
+//		System.out.println(Arrays.toString(newSolution(6,arr1, arr2)));
+		newSolution(6,arr1, arr2);
 	}
+	
+	static String[] newSolution(int n, int[] arr1, int[] arr2) {
+		String[] result = new String[n];
+		for(int i=0; i<n; i++) {
+			result[i] = Integer.toBinaryString(arr1[i] | arr2[i]).replace('0', ' ').replace('1', '#');
+			while(result[i].length() != n) result[i] = ' ' + result[i];
+			
+			System.out.println(result[i]);
+		}
+		
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	static String[] solution(int n, int[] arr1, int[] arr2) {
 		String[][] map = new String[n][n];
@@ -44,6 +81,8 @@ public class SecretMap {
 				}
 			}
 		}
+		StringBuilder s= new StringBuilder("*");
+		
 		return map;
 	}
 }
