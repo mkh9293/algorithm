ce6d718b1c2a2617c89a48b9ca7660dbe25ec893
GwangHun Moon <mkh9293@gmail.com>
2018-09-12 21:32:11 +0900

    resolved Cahce algorithm

--- src/hackerrank/mkh/programmers/Cache.java
+++ src/hackerrank/mkh/programmers/Cache.java
@@ -6,53 +6,36 @@ import java.util.Queue;
 
 public class Cache {
 	public static void main(String[] args) {
-		String[] arr = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
-//		String[] arr = {Jeju, Pangyo, Seoul, Jeju, Pangyo, "Seoul", "Jeju", "Pangyo", "Seoul"};
-		System.out.println(solution(3, arr));
+//		String[] arr = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+		String[] arr = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+
+		System.out.println(solution(2, arr));
 	}
 	
 	static int solution(int cacheSize, String[] cities) {
-		Queue<String> queue = new LinkedList<>();
-		HashMap<String,Integer> map = new HashMap<>();
-		
-		int miss = 5;
-		int hit = 1;
-		int answer = 0;
-		String city = "";
-		
-		for(int i=0; i<cities.length; i++) {
-			
-			city = cities[i].toLowerCase(); 
-			if(queue.contains(city)) {
-				queue.remove();
-				queue.add(city);
-//				map.put(city, (int)map.getOrDefault(city, 0)+1);
-				answer += hit;
-				
-			} else {
-				answer += miss;
-//				map.put(city,0);
-				queue.add(city);
-				if(queue.size() > cacheSize) {
-					queue.remove();
-				}
-			}
-		}
-		
-		return answer;
+		if(cacheSize == 0) return 5 * cities.length;
+	      
+	      int answer = 0;
+	      int hit = 1;
+	      int miss = 5;
+	      String city;
+	      
+	      Queue<String> q = new LinkedList<>();
+	      
+	      for(int i=0; i<cities.length; i++) {
+	          city = cities[i].toLowerCase();
+	          if(q.contains(city)) {
+	              answer += hit;
+	              q.remove(city);
+	              q.add(city);
+	              
+	          } else{
+	              answer += miss;
+	              q.add(city);    
+	              if(cacheSize < q.size()) q.remove();
+	          }
+	      }
+	      
+	      return answer;
 	}
-	
-//	static String getLowHit(Queue<String> queue, HashMap<String, Integer> map) {
-//		String item = queue.peek();
-//		boolean result = false;
-//		
-//		for(String s : queue) {
-//			if(map.get(s) < map.get(item)) {
-//				item = s;
-//				result = true;
-//			}
-//		}
-//
-//		return (result) ? item : queue.peek();
-//	}
 }
