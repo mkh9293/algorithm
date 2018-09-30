b0cb846818602e0dc7cf53bd20ff8c51cd2fa4a9
GwangHun Moon <mkh9293@gmail.com>
2018-09-30 21:29:05 +0900

    resolved FindwayGame algorithm
    resolved findwayGame,
    resolving BilboardAdbert algorithm

---
+++ src/hackerrank/mkh/goorm/BillboardAdbvert.java
@@ -0,0 +1,79 @@
+package hackerrank.mkh.goorm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BillboardAdbvert {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int size = scan.nextInt();
+		int lotate = scan.nextInt();
+		scan.nextLine();
+		
+		String[][] element = new String[size][size];
+		for(int i=0; i<size; i++) {
+			for(int j=0; j<size ;j++) {
+				element[i][j] = scan.next();
+			}
+		}
+		
+		rotateArr(element, lotate);
+		
+		for(int i=0; i<size; i++) {
+			for(int j=0; j<size ;j++) {
+				System.out.print(element[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		// lotate test
+//		lotate = -2;
+//		size = 5;
+//		String[] test = {"김연아", "김아연","아연김","아김연","김김아"};
+//		int n = lotate;
+//		 if(n < 0) // rotating left?
+//	      {
+//	          n = -n % size; // convert to +ve number specifying how 
+//	                                // many positions left to rotate & mod
+//	          n = size - n;  // rotate left by n = rotate right by length - n
+//	      }
+//		 	String[] result = new String[size];
+//	      for(int i = 0; i < size; i++){
+//	    	  System.out.println("count : "+(i+n) % size);
+//	          result[(i+n) % size ] = test[i];
+//	      }
+//	      
+//	      System.out.println(Arrays.toString(result));
+	}
+	
+	static String[][] rotateArr(String[][] a, int lotate) {
+        int n = a.length;
+        int num = 0;
+        for(int i = 0; i < n / 2; i++){
+            for(int j = i; j < n-i-1; j++){
+            	if(i%2 == 0 && lotate > 0) {
+            		// 오른쪽으로 lotate
+            		num = (i+lotate) % n;
+            	} else {
+            		// 왼쪽으로 lotate
+            		num = (i+(n-lotate)) % n; 
+            	}
+            	
+            	System.out.println("num : "+num);
+                String temp = a[i][j];
+//                a[i][j] = a[n-j-1][i];
+                a[i][j] = a[num-j-1][i];
+                a[num-j-1][i] = a[num-1-i][num-1-j];
+                a[num-1-i][num-1-j] = a[j][num-1-i];
+                a[j][num-1-i] = temp;
+//                a[n-j-1][i] = a[n-1-i][n-1-j];
+//                a[n-1-i][n-1-j] = a[j][n-1-i];
+//                a[j][n-1-i] = temp;
+            }
+        }
+        return a;
+    }
+	
+}
---
+++ src/hackerrank/mkh/programmers/FindWayGame.java
@@ -0,0 +1,114 @@
+package hackerrank.mkh.programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindWayGame {
+	public static void main(String[] args) {
+		int[][] nodeinfo = {
+							{5,3},{11,5},{13,3},
+							{3,5},{6,1},{1,3},
+							{8,6},{7,2},{2,2}
+							};
+		
+		solution(nodeinfo);
+	}
+	
+	public static int[][] solution(int[][] nodeinfo){
+		Map<Integer, int[]> map = new HashMap<>();
+		for(int i=0; i<nodeinfo.length; i++) {
+			map.put(i, nodeinfo[i]);
+		}
+		List<Integer> list = new ArrayList<>();
+		
+		list.addAll(map.keySet());
+		
+		Collections.sort(list, new Comparator() {
+			@Override
+			public int compare(Object o1, Object o2) {
+				int[] v1 = map.get(o1);
+				int[] v2 = map.get(o2);
+				
+				if(v1[1] > v2[1]) {
+					return -1;
+				} else if (v1[1] < v2[1]) {
+					return 1;
+				} else {
+					if(v1[0] > v2[0]) return 1;
+					else return -1;
+				}
+			}
+		});
+		
+		int index = list.get(0);
+		int x = 0;
+		Node node = new Node(index+1, map.get(index)[0]);
+		for(int i=1; i<list.size();i++) {
+			index = list.get(i);
+			x = map.get(index)[0];
+			addNode(node, index, x);
+		}
+		
+		List<Integer> preList = new ArrayList<>();
+		List<Integer> postList = new ArrayList<>();
+		
+		preOrder(node, preList);
+		postOrder(node, postList);
+		
+		int[][] answer = new int[2][nodeinfo.length];
+		for(int i=0; i<preList.size(); i++) {
+			answer[0][i] = preList.get(i);
+		}
+		for(int i=0; i<postList.size(); i++) {
+			answer[1][i] = postList.get(i);
+		}
+		
+		return answer;
+	}
+	
+	public static Node addNode(Node node, int val, int x) {
+		if(node == null) {
+			Node tempNode = new Node(val+1, x);
+			return tempNode;
+		} else if (node.x > x) {
+			node.left = addNode(node.left, val, x);
+		} else {
+			node.right = addNode(node.right, val, x);
+		}
+		return node;
+	}
+	
+	public static void preOrder(Node node, List preList) {
+		if(node != null) {
+			preList.add(node.val);
+			preOrder(node.left, preList);
+			preOrder(node.right, preList);
+		}
+	}
+	
+	public static void postOrder(Node node, List postList) {
+		if(node != null) {
+			postOrder(node.left, postList);
+			postOrder(node.right, postList);
+			postList.add(node.val);
+		}
+	}
+}
+
+class Node {
+	public int val;
+	public int x;
+	public Node left;
+	public Node right;
+	
+	public Node(int val, int x) {
+		this.val  = val;
+		this.x    = x;
+		this.left = null;
+		this.right = null;
+	}
+}
